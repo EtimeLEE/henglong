2b110254425a285af024106e757a84523fdb6c76
EtimeLEE <1015607255@qq.com>
2017-01-09 20:30:44 +0800

    9号晚上在家修改

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ $(function() {
 	    // direction: 'vertical',
 	    loop: true,
 	    // 如果需要分页器
-	    pagination: '.swiper-pagination',
+	    // pagination: '.swiper-pagination',
 	    paginationClickable :true,
 	    autoplay: 5000,
   	});
